c1c917f3754142a0c45cf12c858eea7f40d53b5e
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-05-25 02:11:25 -0400

    created Constants.cs, interfaces and animal class

---
+++ ReptileCollection/Constants.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class Constants
+{
+    enum Gender
+    {
+        Female,
+        Male,
+        Unsexed
+    }
+
+    enum WeightMeasurements
+    {
+        Grams,
+        Kilograms,
+        Ounces,
+        Pounds
+    }
+
+    enum Months
+    {
+        January,
+        February,
+        March,
+        April,
+        May, 
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+    enum ReptileType
+    {
+        Snake,
+        Lizard,
+        Chameleon,
+        Gecko,
+        Turtle,
+        Tortoise
+    }
+
+    enum AmphibianType
+    {
+        Frog,
+        Toad,
+        Salamander
+    }
+}
